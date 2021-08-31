373d45359f1a534086b566474cca632899973f16
Olehgan <o.efimov36@mail.ru>
2021-08-31 13:29:53 +0300

    add new function

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  debugger
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <Todolist/>
+        <Todolist/>
+        <Todolist/>
+      </div>
   );
+}
+
+function Todolist() {
+  debugger
+  return <div>
+    <h3> What to learn</h3>
+    <div>
+      <input/>
+      <button>+</button>
+    </div>
+    <ul>
+      <li><input type="checkbox" checked={true} /> <span>CSS&HTML</span></li>
+      <li><input type="checkbox" checked={true} /> <span>JS</span></li>
+      <li><input type="checkbox" checked={true} /> <span>React</span></li>
+    </ul>
+    <div>
+      <button>All</button>
+      <button>Active</button>
+      <button>Completed</button>
+    </div>
+  </div>
 }
 
 export default App;

4bdfe2cb1776f31716cbbe1e1b3585ffc7548db1
Olehgan <o.efimov36@mail.ru>
2022-01-06 11:41:44 +0300

    add CreatTask component to taks-api-stories.tsx

--- src/tasks-api.stories.tsx
+++ src/tasks-api.stories.tsx
@@ -16,4 +16,17 @@ export const GetTasks = () => {
     }, [])
     return <div> {JSON.stringify(state)}</div>
 }
+export const CreateTask = () => {
+    const todolistId = "21c101b8-2448-401d-984d-b7bd4f612cee"
+    const title = 'JS'
 
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        taskApi.creatTask(todolistId, title).then((res) => {
+            debugger
+            setState(res.data)
+        })
+    }, [])
+
+    return <div> {JSON.stringify(state)}</div>
+}

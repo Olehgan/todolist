64aecb8840e4793503ecab61b86976ae6e3248d9
Olehgan <o.efimov36@mail.ru>
2022-01-16 22:13:33 +0300

    add error-utils.ts

---
+++ src/utils/error-utils.ts
@@ -0,0 +1,17 @@
+import {AppActionsType, setAppErrorAC, setAppStatusAC} from "../app/app-reducer";
+import {Dispatch} from "redux";
+import {ResponseType} from "../api/todolists-api";
+
+export const handleServerNetworkError = (dispatch: Dispatch<AppActionsType>, message: string) => {
+    dispatch(setAppStatusAC("failed"))
+    dispatch(setAppErrorAC(message))
+}
+
+export const handleServerAppError =<T>(dispatch: Dispatch<AppActionsType>, data: ResponseType <T>) => {
+    if (data.messages.length) {
+        dispatch(setAppErrorAC(data.messages[0]))
+    } else {
+        dispatch(setAppErrorAC(' Unknown error'))
+    }
+    dispatch(setAppStatusAC("failed"))
+}
